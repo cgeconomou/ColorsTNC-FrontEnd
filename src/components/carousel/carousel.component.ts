936d1ef62717f235adc712433c3f385b350512c6
git_Nikos <nikosoikonomou@gmail.com>
2022-09-23 20:39:57 +0300

    Presentation Day

--- src/components/carousel/carousel.component.ts
+++ src/components/carousel/carousel.component.ts
@@ -7,9 +7,9 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CarouselComponent implements OnInit {
 
-  firstImg:string = '../assets/carouselimages/slide1.jpg'
+  firstImg:string = '../assets/carouselimages/slide11.png'
   secondImg:string = '../assets/carouselimages/slide2.jpg'
-  thirdImg:string = '../assets/carouselimages/slide3.jpg'
+  thirdImg:string = '../assets/carouselimages/slide13.png'
   images = [];
   
   constructor() { }
