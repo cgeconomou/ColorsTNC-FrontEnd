117344b08e619cbb5b3dfb2a7f003e092136e721
Athanasios Mastrogiannis <mastrogiannis.ath@gmail.com>
2022-09-23 05:23:37 +0300

    3StageCreateFormula working in front, not sending the ProductArray to the database

--- src/components/modals/formula/create-formula/create-formula.component.ts
+++ src/components/modals/formula/create-formula/create-formula.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, OnDestroy, OnInit } from '@angular/core';
+import { Component, ElementRef, Input, OnDestroy, OnInit, ViewChild } from '@angular/core';
 import { FormulaService } from 'src/components/formula/formula.service';
 import { Formula } from 'src/components/models/formula';
 import { CreateFormulaService } from './create-formula.service';
@@ -8,7 +8,8 @@ import { FormulaComponent } from 'src/components/formula/formula.component';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Photo } from 'src/components/models/formulaPhoto';
 import { Observable, Subscriber } from 'rxjs';
-import { DomSanitizer } from '@angular/platform-browser';
+import { WarehouseProduct } from 'src/components/models/warehouseProduct';
+import { WarehouseProductService } from 'src/components/warehouse-product/warehouse-product.service';
 
 
 @Component({
@@ -19,64 +20,123 @@ import { DomSanitizer } from '@angular/platform-browser';
 export class CreateFormulaComponent implements OnInit, OnDestroy {
 
   @Input() formulaProducts!: Formula[];
-  selectedProducts!: Product[];
+  selectedProducts: Product[]=[];
   productsBrands!: string[];
-  products!: Product[];
+  
   productTest!: Product[];
   emptyImageUrl: string = "assets/Images/uploadPhoto.jpg";
   selectedFile!: File[];
   image = new Array<string>(4);
-  filedata !:FormData;
-  photoFormulaid!:number;
-  photoFormulaFile!:any;
-  photo!:Photo;
-  photoUrl!:string;
+  filedata !: FormData;
+  photoFormulaid!: number;
+  photoFormulaFile!: any;
+  photo!: Photo;
+  photoUrl!: string;
   formulaImage!: string;
   showExtraProduct: boolean = false;
   testUrlPhoto!: string;
-  
-  showStage1Main:boolean= true;
-  showStage2Products:boolean= false;
-  showStage3Photo:boolean= false;
+////////////////////////////////////////////////
+  showStage1Main: boolean = true;
+  showStage2Products: boolean = false;
+  showStage3Photo: boolean = false;
+  formula!:any;
+  formulaName!:string;
+  formulaCost!:number;
+  formulaDuration!:string;
+  formulaServiceType!:string;
+  formulaPhotosUrl!:string;
+  products!: Product[];
+  formulaCreationDate!:Date;
+  formulaId!:number;
+  warehouseProducts!:WarehouseProduct[];
+  selectedWarehouseProducts:WarehouseProduct[]= [];
+  selectedWarehouseProductsQuantities:number[] =[];
+  tempProduct!:Product;
 
-  constructor(private domService: DomSanitizer ,private formulaComponent: FormulaComponent, public createFormulaService: CreateFormulaService, private formulaService: FormulaService, public photoService: UploadPhotoComponent | null,private http : HttpClient) { }
+  constructor(private formulaComponent: FormulaComponent, public createFormulaService: CreateFormulaService,private formulaService:FormulaService, private warehouseProductService: WarehouseProductService, public photoService: UploadPhotoComponent | null, private http: HttpClient) { }
 
-  getFormulaProduct(e:any){
+  ShowActivePage1() {
+    this.showStage1Main = true;
+    this.showStage2Products = false;
+    this.showStage3Photo = false;
+  }
+  ShowActivePage2() {
+    this.showStage1Main = false;
+    this.showStage2Products = true;
+    this.showStage3Photo = false;
+  }
+  ShowActivePage3() {
+    this.showStage1Main = false;
+    this.showStage2Products = false;
+    this.showStage3Photo = true;
+  }
+  getFormulaProduct(e: any) {
     console.log(e);
   }
-  
-  CreateFormulaHandler(formulaName: string, formulaServiceType: string, formulaDuration: string, formulaCost: number): void {
+
+  // formulaName: string, formulaServiceType: string, formulaDuration: string, formulaCost: number
+  CreateFormulaHandler(): void {
+    
     this.createFormulaService.showCreateFormulaForm = false;
     let formulaDate;
-    
-    this.formulaService.CreateFormula({ FormulaName: formulaName, CreationDate: formulaDate,Cost: formulaCost, Duration: formulaDuration, ServiceType: formulaServiceType, FormulasPhotosid: this.photoFormulaid,FormulasPhotosUrl:this.testUrlPhoto, Products: this.selectedProducts } as Formula)
+
+    this.formulaService.CreateFormula({ FormulaName: this.formulaName, CreationDate: formulaDate, Cost: this.formulaCost, Duration: this.formulaDuration, ServiceType: this.formulaServiceType, FormulasPhotosid: this.photoFormulaid, FormulasPhotosUrl: this.testUrlPhoto, Products: this.selectedProducts } as Formula)
       .subscribe(
         {
-          next: response => {console.log(response)},
+          next: response => { console.log("Next ", response) },
           error: error => console.log(error),
-          complete: () => { console.log("Create Done"),this.formulaComponent.GetAllFormulasHandler()}
+          complete: () => { console.log("Create Done"), this.formulaComponent.GetAllFormulasHandler() }
         }
       )
   };
+  GetWarehouseProductsHandler(): WarehouseProduct[] {
+    this.warehouseProductService.GetWarehouseProducts().subscribe(
+      {
+        next: response => { this.warehouseProducts = response },
+        error: error => console.log(error) ,
+        complete: () => { console.log("WarehouseProducts Done")}
+      }
+    )
+    return this.warehouseProducts;
+  }
 
-  UploadPhotoHandler(){
-    const filedata=new FormData();
-    for(var i=0; i<this.selectedFile.length; i++){
+  CreateFormulaStage1Form(): void{
+    this.ShowActivePage2();
+  }
+  
+  // i:number =0;
+  
+  CreateFormulaStage2Products(){
+    for(let i=0; i<this.selectedWarehouseProducts.length; i++){
+      let temp:any =this.selectedWarehouseProducts[i];
+      let arr:string =temp.split('--');
+      let brand = arr[0];
+      let colorCode = arr[1];
+      let usedQuantity = this.selectedWarehouseProductsQuantities[i];
+      this.tempProduct =({Brand:brand,ColorCode:colorCode, UsedQuantity:usedQuantity})as Product;
+      this.selectedProducts.push(this.tempProduct); 
+    }
+    console.log(this.selectedProducts);
+    this.ShowActivePage3();
+  }
+  
+  UploadPhotoHandler() {
+    const filedata = new FormData();
+    for (var i = 0; i < this.selectedFile.length; i++) {
       filedata.append('image', this.selectedFile[i], this.selectedFile[i].name);
     }
     this.http.post('https://localhost:44321/api/Photo/UploadFile', filedata)
-    .subscribe(
-      {
-        next: response =>{console.log(response), this.testUrlPhoto = response.toString()},
-        error: error => console.log(error),
-        complete: () => { console.log("Create Done"),console.log(this.testUrlPhoto) }
-      }  
-    )
+      .subscribe(
+        {
+          next: response => { console.log(response), this.testUrlPhoto = response.toString() },
+          error: error => console.log(error),
+          complete: () => { console.log("Create Done"), console.log(this.testUrlPhoto) }
+        }
+      )
   }
 
-  
   setSingleImage(imageId: number): string {
-   
+
     const headers = new HttpHeaders();
     this.http.get('https://localhost:44321/api/ImageFormula?id=' + imageId, { headers, responseType: 'blob' })
       .subscribe((data: Blob) => {
@@ -94,17 +154,17 @@ export class CreateFormulaComponent implements OnInit, OnDestroy {
           console.log(img)
         });
       });
-      console.log("autp eiani to this formula")
-      
-      console.log(this.formulaImage)
+    console.log("autp eiani to this formula")
+
+    console.log(this.formulaImage)
     return this.formulaImage;
   }
 
-// GettingPhotoId(){
-//   let photos  =this.UploadPhotoHandler() as Photo;
-//   this.photoFormulaid = photos.Id;
-//   console.log(this.photoFormulaid);
-// }
+  // GettingPhotoId(){
+  //   let photos  =this.UploadPhotoHandler() as Photo;
+  //   this.photoFormulaid = photos.Id;
+  //   console.log(this.photoFormulaid);
+  // }
 
   fileToUpload: any;
   handleFileInput(file: FileList) {
@@ -118,20 +178,20 @@ export class CreateFormulaComponent implements OnInit, OnDestroy {
 
   OnFileSelected(event: any) {
     this.selectedFile = <File[]>event.target.files;
-
   }
 
   GettingProducts(): void {
     this.products = this.formulaComponent.GetProducts()
   }
 
-  ShowProductToAdd(): void{
+  ShowProductToAdd(): void {
     this.showExtraProduct = !this.showExtraProduct;
     console.log(this.showExtraProduct);
   }
 
   ngOnInit(): void {
-    this.GettingProducts();
+    //this.GettingProducts();
+    this.GetWarehouseProductsHandler();
   }
 
   ngOnDestroy(): void {
